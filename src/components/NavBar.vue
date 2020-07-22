113abcc8a0d9a71fb4d0e21b674f9dd7c8db492a
Oketa Fred <oketafred@gmail.com>
2020-07-22 18:35:02 +0300

    Added Navigation Menu

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">Oketa Fred</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>        
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-nav-item href="#">Twitter</b-nav-item>
+            <b-nav-item href="#" >Resume</b-nav-item>
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
