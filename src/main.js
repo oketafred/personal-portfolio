6acd3953633c4e48c670634adf74824e2e4be903
Oketa Fred <oketafred@gmail.com>
2020-07-22 20:09:58 +0300

    Added Navigation Menu (#1)
    * Added Bootstrap Vue
    
    * Added Navigation Menu
    
    * Added Fontawesome Icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "font-awesome/css/font-awesome.min.css"
+
+Vue.component('VueFontawesome', require('vue-fontawesome-icon/VueFontawesome.vue').default);
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <b-container class="text-center">
+            <h1>Coming Soon</h1>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
